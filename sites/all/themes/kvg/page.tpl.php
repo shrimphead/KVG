9b72f362b3f814a5396cadc956efbeee532b2475
Kendrick <wallncrown@mac.com>
2012-02-13 11:44:08 -0800

    Facebook Integration - fb_social module
    - block config + fb ID #s required.

--- sites/all/themes/kvg/page.tpl.php
+++ sites/all/themes/kvg/page.tpl.php
@@ -10,6 +10,16 @@
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 </head>
 
 <body class="<?php print $body_classes; ?> show-grid">
